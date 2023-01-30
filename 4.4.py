932f0782f2e6dd7483b33e29cbf84ed6a3604987
Paw1915 <piotrandrzejwalczak@gmail.com>
2023-01-30 21:09:13 +0100

    Add rest of operations

--- 4.4.py
+++ 4.4.py
@@ -10,4 +10,19 @@ def addition(x, y):
     logging.debug(f"Dodaję %g i %g", x, y)
     addition_result = x + y
     print(f"Wynik to {addition_result:g}")
+
+def subtraction(x, y):
+    logging.debug(f"Odejmuję %g od %g", x, y)
+    subtraction_result = x - y
+    print(f"Wynik to {subtraction_result:g}")
+
+def multiplication(x, y):
+    logging.debug(f"Mnożę %g i %g", x, y)
+    multiplication_result = x * y
+    print(f"Wynik to {multiplication_result:g}")
+    
+def division(x, y):
+    logging.debug(f"Dzielę %g przez %g", x, y)
+    division_result = x / y
+    print(f"Wynik to {division_result:g}")
     
