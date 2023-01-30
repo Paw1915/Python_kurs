c75caec274f95ca06eabe05a26b4f551375df945
Paw1915 <piotrandrzejwalczak@gmail.com>
2023-01-30 21:00:18 +0100

    Add addition, input and import libraries

--- 4.4.py
+++ 4.4.py
@@ -1,8 +1,13 @@
 import sys
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(message)s',)
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
-operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie")
-x = input("Podaj składnik 1:")
-y = input("Podaj składnik 2:")
+operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+x = float(input("Podaj składnik 1: "))
+y = float(input("Podaj składnik 2: "))
 
+def addition(x, y):
+    logging.debug(f"Dodaję %g i %g", x, y)
+    addition_result = x + y
+    print(f"Wynik to {addition_result:g}")
+    
