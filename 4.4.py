a5f84923e73b4070d9a1008877fd2c3a26d59567
Paw1915 <piotrandrzejwalczak@gmail.com>
2023-01-30 19:09:34 +0100

    Add libraries and inputs

---
+++ 4.4.py
@@ -0,0 +1,8 @@
+import sys
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(message)s',)
+
+operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie")
+x = input("Podaj składnik 1:")
+y = input("Podaj składnik 2:")
+
