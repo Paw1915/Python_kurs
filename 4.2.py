81e30d67d1587efed52a2a96405192b5a354c0f2
Paw1915 <piotrandrzejwalczak@gmail.com>
2023-01-30 16:11:18 +0100

    Add documantation

--- 4.2.py
+++ 4.2.py
@@ -1,3 +1,7 @@
 def palindrome_check(word):
+    '''
+    Checks if given word is a palindrome;
+    Works only with words, not sentences
+    '''
     return word == word[::-1]
 print(palindrome_check("racecar"))
